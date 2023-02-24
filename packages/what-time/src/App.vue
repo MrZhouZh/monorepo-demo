4db18da7a381f9f92e7d3a91e434ddcc5f803f34
MrZhouZh <17600330793@sina.cn>
2023-02-24 22:06:03 +0800

    feat: what-time setup

---
+++ packages/what-time/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>Hello Chow</div>
+</template>
+
+<style>
+html {
+  background-color: #222;
+  color: #fff;
+  color-scheme: dark;
+}
+</style>
