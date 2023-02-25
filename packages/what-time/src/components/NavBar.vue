8bbd38dc681150e61a871c18150c903d82043645
MrZhouZh <17600330793@sina.cn>
2023-02-25 20:09:12 +0800

    feat: auto import version down

---
+++ packages/what-time/src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div flex="~ gap2" text-xl items-center px8 pt8 pb2>
+    <h1 text-2xl font-serif op50>
+      <span font-bold>What Time</span> works for you?
+    </h1>
+    <div flex-auto />
+    <a icon-btn i-carbon-logo-github href="https://github.com/MrZhouZh/monorepo-demo/tree/main/packages/what-time" target="_blank" title="Github" />
+    <DarkSwitcher />
+  </div>
+</template>
