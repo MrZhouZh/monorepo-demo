139cc2221b41da929aaab6a763e643786e6385b0
MrZhouZh <17600330793@sina.cn>
2023-04-26 17:11:44 +0800

    feat: mount container node

--- packages/vue-ssr-01/server.js
+++ packages/vue-ssr-01/server.js
@@ -1,32 +1,37 @@
 import express from 'express'
-import { createSSRApp } from 'vue'
+import { createApp } from './app.js'
 import { renderToString } from 'vue/server-renderer'
 
 const server = express()
 
 server.get('/', (req, res) => {
-  const app = createSSRApp({
-    data: () => ({ count: 1 }),
-    template: `<button @click="count++">{{ count }}</button>`,
-  })
+  const app = createApp()
 
   renderToString(app).then(html => {
-    res.send(`
-    <!DOCTYPE html>
+    res.send(`<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Vue SSR Example</title>
+      <script type="importmap">
+      {
+        "imports": {
+          "vue": "https://unpkg.com/vue@3.2.36/dist/vue.esm-browser.js"
+        }
+      }
+      </script>
+      <script type="module" src="/client.js"></script>
     </head>
     <body>
       <div id="app">${html}</div>
     </body>
-    </html>
-    `)
+    </html>`)
   })
 })
+
+server.use(express.static('.'))
 
 server.listen(3000, () => {
   console.log(`server is running at http://localhost:3000`)
